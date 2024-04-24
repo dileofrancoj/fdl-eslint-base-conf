4e54d62c3de87e1bb4b55e3cf7d017d96d73a4bd
Franco Di Leo <dileo.francoj@gmail.com>
2024-04-23 22:34:14 -0300

    chore: add globals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 module.exports = {
     extends: ['standard-with-typescript', 'eslint-config-important-stuff'],
     plugins: ['simple-import-sort', 'unused-imports'],
+    env: {
+      node: true,
+    },
+    globals: {
+      "NodeJS": true
+    },
     rules: {
       'simple-import-sort/exports': "error",
       "simple-import-sort/imports": "error",
